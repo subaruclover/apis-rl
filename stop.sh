d2fa6d7582c737e267d5fa5d1b2fdb84cdd07db6
SUBARU <subaru6725@hotmail.com>
2021-12-27 13:04:19 +0900

    add venv, stop shell script; env requirements.txt

---
+++ stop.sh
@@ -0,0 +1,20 @@
+echo 'call stop.sh'
+
+# TODO edit stop.sh file : stop for one/all houses
+get_pids() {
+ ps -f -U `whoami` | grep main.py | grep -v 'grep main.py' | grep -v 'stop.sh' | while read _USER_ _PID_ _OTHERS_ ; do
+  echo $_PID_
+ done
+}
+
+while true; do
+ _PIDS_=`get_pids`
+ if [ -z "$_PIDS_" ] ; then
+  break
+ fi
+ echo kill $_PIDS_
+ kill $_PIDS_
+ sleep 1
+done
+
+echo '... done'
